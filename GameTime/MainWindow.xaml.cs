2cb02041c483869ac65cb6125c19b045394d0ea2
Stuk_uc23@student.itstep.org <Stuk_uc23@student.itstep.org>
2023-04-07 19:44:50 +0300

    change

--- GameTime/MainWindow.xaml.cs
+++ GameTime/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace GameTime
 {
@@ -25,29 +14,13 @@ namespace GameTime
             InitializeComponent();
         }
 
-        private void Polygon_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
-                DragMove();
-        }
-
-        private void minimizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
-
-        private void maximizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            if(WindowState == WindowState.Normal)
-                WindowState = WindowState.Maximized;
-            else
-                WindowState = WindowState.Normal;
-        }
-
-        private void closeButton_Click(object sender, RoutedEventArgs e)
-        {
-            //Close the App
-            Close();
+            {
+                this.DragMove();
+            }
+                
         }
     }
 }
